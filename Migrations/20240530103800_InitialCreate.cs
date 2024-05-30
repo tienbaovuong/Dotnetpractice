325a38058b719272400cb957fef4c844a98d7b52
bao <tienbaoams@gmail.com>
2024-05-31 04:41:05 +1000

    Create migration

---
+++ Migrations/20240530103800_InitialCreate.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dotnetpractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CityName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostCode = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suburb",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SuburbName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suburb", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Suburb_City_CityId",
+                        column: x => x.CityId,
+                        principalTable: "City",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Street",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StreetName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SuburbId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Street", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Street_Suburb_SuburbId",
+                        column: x => x.SuburbId,
+                        principalTable: "Suburb",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "House",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OwnerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HouseNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OccupantCount = table.Column<int>(type: "int", nullable: false),
+                    HouseArea = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StreetId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_House", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_House_Street_StreetId",
+                        column: x => x.StreetId,
+                        principalTable: "Street",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_House_StreetId",
+                table: "House",
+                column: "StreetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Street_SuburbId",
+                table: "Street",
+                column: "SuburbId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suburb_CityId",
+                table: "Suburb",
+                column: "CityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "House");
+
+            migrationBuilder.DropTable(
+                name: "Street");
+
+            migrationBuilder.DropTable(
+                name: "Suburb");
+
+            migrationBuilder.DropTable(
+                name: "City");
+        }
+    }
+}
---
+++ Migrations/20240530163614_Latest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dotnetpractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class Latest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CityName",
+                table: "City",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_City_CityName",
+                table: "City",
+                column: "CityName",
+                descending: new bool[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_City_CityName",
+                table: "City");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CityName",
+                table: "City",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
